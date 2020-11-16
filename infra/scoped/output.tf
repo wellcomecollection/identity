6b0966fa0fb0460c78e133875ca81c443ce7a447
Daniel Grant <daniel.grant@digirati.com>
2020-11-16 16:14:34 +0000

    Added initial Auth0 -> Sierra DB and dummy test Auth0 client

--- infra/scoped/output.tf
+++ infra/scoped/output.tf
@@ -3,3 +3,13 @@
 output "api_key-dummy" {
   value = aws_api_gateway_api_key.dummy.value
 }
+
+# Auth0 Clients
+
+output "auth0_client-dummy-client_id" {
+  value = auth0_client.dummy_test.client_id
+}
+
+output "auth0_client-dummy-client_secret" {
+  value = auth0_client.dummy_test.client_secret
+}
