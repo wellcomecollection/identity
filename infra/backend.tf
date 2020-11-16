1490b26af9cba09123a4ed722168c22225975681
Daniel Grant <daniel.grant@digirati.com>
2020-11-16 09:21:47 +0000

    Switching TF remote state bucket

--- infra/backend.tf
+++ infra/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "= 0.13.5" # Pin to a specific version to avoid accidental upgrading of the statefile
 
   backend "s3" {
-    bucket = "identity-tf-remote-state"
+    bucket = "identity-remote-state"
     key    = "terraform.tfstate"
     region = "eu-west-1"
   }
