dbef7daf3a863519d44f36dcb1d82746c9c4f727
Gary Tierney <gary.tierney@fastmail.com>
2020-11-19 18:00:24 +0000

    Add logging to lambda packaging

--- .buildkite/scripts/aws-lambda-packaging.sh
+++ .buildkite/scripts/aws-lambda-packaging.sh
@@ -6,20 +6,23 @@ function __package_lambda() {
   local bundle_name="$2"
   local zip_file="$bundle_name-${BUILDKITE_BRANCH/\//-}.zip"
 
+  echo "Packaging lambdas"
+  # shellcheck disable=SC2086
+  echo $package_path
+  echo "$bundle_name"
+  echo "$zip_file"
+
   {
     rm "$package_path/$zip_file"
 
     cd "$package_path/lib"
-    rm "../$zip_file"
-
     zip -r "../$zip_file" ./
     cd ../
     zip -ur "$zip_file" node_modules
   }
 
-  aws s3 cp "$package_path/.zip" "s3://identity-dist/${zip_file}"
+  aws s3 cp "$package_path/$zip_file" "s3://identity-dist/${zip_file}"
 }
-
 
 __package_lambda "packages/apps/api" "identity-api"
 __package_lambda "packages/apps/api-authorizer" "identity-authorizer"
