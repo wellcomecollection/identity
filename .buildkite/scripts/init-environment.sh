6abb59e8a03fd9ee9d80f6b8d1dce12c8de38776
Daniel Grant <daniel.grant@digirati.com>
2020-11-20 14:25:07 +0000

    Testing independent Buildkite pipeline

--- .buildkite/scripts/init-environment.sh
+++ .buildkite/scripts/init-environment.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "$BUILDKITE_SOURCE" == "ui" ]; then
+  export DEPLOY_ENVIRONMENT=$(buildkite-agent meta-data get deploy-environment)
+  export DEPLOY_API_GATEWAY_STAGE=$(buildkite-agent meta-data get deploy-api-gateway-stage)
+fi
+
 cd infra/scoped && terraform init && terraform workspace select stage
 terraform output -json -no-color | jq -r .ci_environment_variables.value[] > env.sh
 chmod +x env.sh && source env.sh && rm env.sh
