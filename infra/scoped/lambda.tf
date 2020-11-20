241172ff28a0d0080e4f5d946336b13350e3c55d
Gary Tierney <gary.tierney@fastmail.com>
2020-11-20 11:39:21 +0000

    Add WIP deployment script

--- infra/scoped/lambda.tf
+++ infra/scoped/lambda.tf
@@ -5,7 +5,7 @@ resource "aws_lambda_function" "authorizer" {
   handler       = "index.lambdaHandler"
   role          = aws_iam_role.identity_api_gateway_lambda_role.arn
   runtime       = "nodejs12.x"
-
+  filename      = "data/empty.zip"
   tags = merge(
     local.common_tags,
     {
@@ -29,6 +29,7 @@ resource "aws_lambda_function" "api" {
   handler       = "server-lambda.lambdaHandler"
   role          = aws_iam_role.identity_api_gateway_lambda_role.arn
   runtime       = "nodejs12.x"
+  filename      = "data/empty.zip"
 
   tags = merge(
     local.common_tags,
