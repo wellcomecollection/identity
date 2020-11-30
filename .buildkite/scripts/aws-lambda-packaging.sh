b1f9054e894c826216abb11ffa3f6de0734152c0
Daniel Grant <daniel.grant@digirati.com>
2020-11-30 10:13:27 +0000

    Minor changes to Lambda packaging script

--- .buildkite/scripts/aws-lambda-packaging.sh
+++ .buildkite/scripts/aws-lambda-packaging.sh
@@ -7,24 +7,24 @@
 set -o errexit
 
 function package_lambda() {
-  local package_path="$1"
-  local bundle_name="$2"
-  local zip_file="$bundle_name-${BUILDKITE_BRANCH/\//-}.zip"
+  local package_path="${1}"
+  local bundle_name="${2}"
+  local zip_file="${bundle_name}-${BUILDKITE_BRANCH/\//-}.zip"
 
   echo "Packaging lambdas"
 
-  echo "$package_path"
-  echo "$bundle_name"
-  echo "$zip_file"
+  echo "${package_path}"
+  echo "${bundle_name}"
+  echo "${zip_file}"
 
   (
-    cd "$package_path/lib" || return
-    zip -r "/app/.buildkite/build/$zip_file" ./
+    cd "${package_path}/lib" || return
+    zip -r "/app/.buildkite/build/${zip_file}" ./
     cd ../
-    zip -ur "/app/.buildkite/build/$zip_file" node_modules
+    zip -ur "/app/.buildkite/build/${zip_file}" node_modules
   )
 
-  aws s3 cp "/app/.buildkite/build/$zip_file" "s3://identity-dist/${zip_file}"
+  aws s3 cp "/app/.buildkite/build/${zip_file}" "s3://identity-dist/${zip_file}"
 }
 
 __package_lambda "/app/packages/apps/api" "identity-api"
