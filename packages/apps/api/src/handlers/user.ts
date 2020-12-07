323e661f5a8c5d5a9f2d83e7c48f9c90625b914a
Daniel Grant <daniel.grant@digirati.com>
2020-12-07 19:45:46 +0000

    Rollback

--- packages/apps/api/src/handlers/user.ts
+++ packages/apps/api/src/handlers/user.ts
@@ -28,31 +28,39 @@ export async function getUser(sierraClient: SierraClient, auth0Client: Auth0Clie
 export async function createUser(sierraClient: SierraClient, auth0Client: Auth0Client, request: Request, response: Response): Promise<void> {
   return sierraClient.getPatronRecordByEmail(request.body.email).then(sierraResponse => {
     if (sierraResponse.status === ResponseStatus.NotFound) {
-      return sierraClient.createPatronRecord(request.body.title, request.body.firstName, request.body.lastName, request.body.password).then(sierraResponse => {
-        if (sierraResponse.status === ResponseStatus.Success) {
-          return auth0Client.createUser(sierraResponse.result, request.body.email, request.body.password).then(auth0Response => {
-            if (auth0Response.status === ResponseStatus.Success) {
-              return sierraClient.addPostCreationFields(sierraResponse.result, request.body.email).then(sierraResponse => {
-                if (sierraResponse.status === ResponseStatus.Success) {
-                  response.status(201).json(toUser(sierraResponse.result, auth0Response.result));
-                } else if (sierraResponse.status === ResponseStatus.MalformedRequest) {
-                  response.status(400).json(toMessage(sierraResponse.message));
+      auth0Client.getProfileByEmail(request.body.email).then(auth0Response => {
+        if (auth0Response.status === ResponseStatus.NotFound) {
+          return sierraClient.createPatronRecord(request.body.title, request.body.firstName, request.body.lastName, request.body.password).then(sierraResponse => {
+            if (sierraResponse.status === ResponseStatus.Success) {
+              return auth0Client.createUser(sierraResponse.result, request.body.email, request.body.password).then(auth0Response => {
+                if (auth0Response.status === ResponseStatus.Success) {
+                  return sierraClient.addPostCreationFields(sierraResponse.result, request.body.email).then(sierraResponse => {
+                    if (sierraResponse.status === ResponseStatus.Success) {
+                      response.status(201).json(toUser(sierraResponse.result, auth0Response.result));
+                    } else if (sierraResponse.status === ResponseStatus.MalformedRequest) {
+                      response.status(400).json(toMessage(sierraResponse.message));
+                    } else {
+                      response.status(500).json(toMessage(sierraResponse.message));
+                    }
+                  });
+                } else if (auth0Response.status === ResponseStatus.MalformedRequest) {
+                  response.status(400).json(toMessage(auth0Response.message));
+                } else if (auth0Response.status === ResponseStatus.UserAlreadyExists) {
+                  response.status(409).json(toMessage(auth0Response.message));
                 } else {
-                  response.status(500).json(toMessage(sierraResponse.message));
+                  response.status(500).json(toMessage(auth0Response.message));
                 }
               });
-            } else if (auth0Response.status === ResponseStatus.MalformedRequest) {
-              response.status(400).json(toMessage(auth0Response.message));
-            } else if (auth0Response.status === ResponseStatus.UserAlreadyExists) {
-              response.status(409).json(toMessage(auth0Response.message));
+            } else if (sierraResponse.status === ResponseStatus.MalformedRequest) {
+              response.status(400).json(toMessage(sierraResponse.message));
             } else {
-              response.status(500).json(toMessage(auth0Response.message));
+              response.status(500).json(toMessage(sierraResponse.message));
             }
           });
-        } else if (sierraResponse.status === ResponseStatus.MalformedRequest) {
-          response.status(400).json(toMessage(sierraResponse.message));
+        } else if (auth0Response.status === ResponseStatus.Success) {
+          response.status(409).json(toMessage('Auth0 user with email [' + request.body.email + '] already exists'));
         } else {
-          response.status(500).json(toMessage(sierraResponse.message));
+          response.status(500).json(toMessage(auth0Response.message));
         }
       });
     } else if (sierraResponse.status === ResponseStatus.Success) {
