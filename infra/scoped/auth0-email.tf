dec67f472ca248fad0093e3ceb9a94123c026ca7
Daniel Grant <daniel.grant@digirati.com>
2020-12-11 09:30:59 +0000

    Minor changes to Auth0 email configuration

--- infra/scoped/auth0-email.tf
+++ infra/scoped/auth0-email.tf
@@ -31,7 +31,7 @@ resource "auth0_email_template" "reset_email" {
   template                = "reset_email"
   enabled                 = true
   from                    = "${aws_ssm_parameter.auth0_email_from_name.value} <${data.aws_ssm_parameter.auth0_email_from_user.value}@${data.aws_ssm_parameter.auth0_email_from_domain.value}>"
-  subject                 = "Welcome to Wellcome Collection"
+  subject                 = "Reset your password"
   body                    = "ignored"
   syntax                  = "liquid"
   url_lifetime_in_seconds = 432000
