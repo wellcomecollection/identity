ef2b02c8e6463b0a8467be2e3487d022e8fab39c
Alan Russell <alan.russell@digirati.com>
2021-01-13 15:04:26 +0000

    Convert WeCo TabNav component to use router Link
    Converted from Flow to TS by @ionab
    Use react-router Link instead of Next Link

---
+++ packages/account-management/src/frontend/AccountManagement/TabNav.tsx
@@ -0,0 +1,103 @@
+// Converted from flow to TSX element
+import React, { ComponentType } from 'react';
+import styled from 'styled-components';
+import { font, classNames } from '@weco/common/utils/classnames';
+import Space, { SpaceComponentProps } from '@weco/common/views/components/styled/Space';
+import { Link } from 'react-router-dom';
+
+export type SelectableTextLink = {
+  text: string;
+  link: any;
+  selected: boolean;
+  onClick?: (HTMLAnchorElement) => void;
+};
+
+type Props = {
+  items: SelectableTextLink[];
+};
+
+const NavItemInner: ComponentType<SpaceComponentProps> = styled(Space).attrs((props: any) => ({
+  className: classNames({
+    selected: props.selected,
+    block: true,
+    relative: true,
+  }),
+}))`
+  z-index: 1;
+  padding: 0 0.3em;
+  &:after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    height: 0.6rem;
+    left: 0;
+    width: 0;
+    background: ${(props) => props.theme.color('yellow')};
+    z-index: -1;
+    transition: width 200ms ease;
+  }
+  &:hover,
+  &:focus {
+    &:after {
+      width: 100%;
+      // Prevent iOS double-tap link issue
+      // https://css-tricks.com/annoying-mobile-double-tap-link-issue/
+      @media (pointer: coarse) {
+        width: 0;
+      }
+    }
+  }
+  &.selected:after {
+    width: 100%;
+  }
+`;
+const NavItem = ({ link, text, selected, onClick }: SelectableTextLink) => (
+  <Link {...link}>
+    <Space
+      v={{
+        size: 'm',
+        properties: ['padding-top', 'padding-bottom'],
+      }}
+      as="a"
+      className={classNames({
+        'plain-link': true,
+        block: true,
+      })}
+      onClick={onClick}
+    >
+      <NavItemInner as="span" h={{ size: 'm', properties: ['margin-right'] }} selected={selected}>
+        {text}
+      </NavItemInner>
+    </Space>
+  </Link>
+);
+
+const TabNav = ({ items }: Props): JSX.Element => {
+  return (
+    <div
+      className={classNames({
+        [font('hnm', 4)]: true,
+      })}
+    >
+      <ul
+        className={classNames({
+          'plain-list no-margin no-padding': true,
+          'flex flex--wrap': true,
+        })}
+      >
+        {items.map((item) => (
+          <li
+            key={item.text}
+            style={{
+              marginRight: '1vw',
+            }}
+          >
+            <NavItem {...item} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TabNav;
