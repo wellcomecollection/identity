7d547904f044e0774211c6698ada6502f06b2863
Daniel Grant <daniel.grant@digirati.com>
2021-01-19 12:18:36 +0000

    Renamed enrich profile rule

--- infra/scoped/auth0-rule.tf
+++ infra/scoped/auth0-rule.tf
@@ -14,7 +14,7 @@ resource "auth0_rule_config" "rule_config_sierra_client_secret" {
 }
 
 resource "auth0_rule" "enrich_patron_attributes" {
-  name    = "Enrich Patron Attributes"
+  name    = "Enrich-Patron-Attributes"
   script  = file("${path.module}/../../packages/apps/auth0-actions/src/enrich_patron_attributes.js")
   enabled = true
 
