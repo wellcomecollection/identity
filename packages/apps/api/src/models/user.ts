76201d682b360827b7f62cbac9f6950f5be2130e
Daniel Grant <daniel.grant@digirati.com>
2021-01-20 13:38:49 +0000

    Wiring Lambda app into search request handler

--- packages/apps/api/src/models/user.ts
+++ packages/apps/api/src/models/user.ts
@@ -30,32 +30,3 @@ interface User {
   lastLoginIp: string,
   totalLogins: number
 }
-
-
-export const DummyUserOne = {
-  patronId: 123456,
-  barcode: '654321',
-  firstName: 'John',
-  lastName: 'Doe',
-  email: 'john.doe@example.com',
-  emailValidated: true,
-  locked: false,
-  creationDate: '2020-11-01 00:00:00',
-  lastLogin: '2020-11-01 12:00:00',
-  lastLoginIp: '127.0.0.1',
-  totalLogins: 120
-};
-
-export const DummyUserTwo = {
-  patronId: 654321,
-  barcode: '123456',
-  firstName: 'Jane',
-  lastName: 'Doe',
-  email: 'jane.doe@example.com',
-  emailValidated: true,
-  locked: false,
-  creationDate: '2020-11-11 00:00:00',
-  lastLogin: '2020-11-11 12:00:00',
-  lastLoginIp: '192.168.0.1',
-  totalLogins: 240
-};
