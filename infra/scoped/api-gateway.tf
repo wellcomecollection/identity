5060758aa75f26760d2b5275020d0d2a2d9cbf6b
Daniel Grant <daniel.grant@digirati.com>
2021-01-20 11:38:05 +0000

    Updating API schema for user searching

--- infra/scoped/api-gateway.tf
+++ infra/scoped/api-gateway.tf
@@ -267,6 +267,23 @@ resource "aws_api_gateway_method_response" "users_get_500" {
   }
 }
 
+# 503 Service Unavailable
+
+resource "aws_api_gateway_method_response" "users_get_503" {
+  rest_api_id = aws_api_gateway_rest_api.identity.id
+  resource_id = aws_api_gateway_resource.users.id
+  http_method = aws_api_gateway_method.users_get.http_method
+  status_code = "503"
+
+  response_models = {
+    "application/json" = "Error"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+}
+
 # [POST]
 
 resource "aws_api_gateway_method" "users_post" {
