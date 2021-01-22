499815d3ac602e75319be186569ddce77e5784ac
rob.march <rob.march@digirati.com>
2021-01-22 17:03:20 +0000

    Invert authentication redirect check to make login the default branch

--- packages/account-management/src/routes/index.ts
+++ packages/account-management/src/routes/index.ts
@@ -5,24 +5,24 @@ const unAuthenticatedPages: string[] = ['/register', '/validated', '/error'];
 export const indexPage: RouteMiddleware = (context) => {
   const bundle = context.routes.url('assets-bundles');
 
-  if (!unAuthenticatedPages.includes(context.request.URL.pathname) && !context.isAuthenticated()) {
-    context.redirect('/login');
+  if (context.isAuthenticated() || unAuthenticatedPages.includes(context.request.URL.pathname)) {
+    console.log('current user ->', context.state.user);
+    context.response.body = `
+      <html lang="en">
+        <head>
+          <title>Account management</title>
+          <script crossorigin src="https://unpkg.com/react@16/umd/react.development.js"></script>
+          <script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"></script>
+        </head>
+        <body>
+          <div id="root"></div>
+          <script type="application/javascript" src="${bundle}"></script>
+        </body>
+      </html>
+    `;
     return;
+
+  } else {
+    context.redirect('/login');
   }
-
-  console.log('current user ->', context.state.user);
-
-  context.response.body = `
-    <html lang="en">
-      <head>
-        <title>Account management</title>
-        <script crossorigin src="https://unpkg.com/react@16/umd/react.development.js"></script>
-        <script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"></script>
-      </head>
-      <body>
-        <div id="root"></div>
-        <script type="application/javascript" src="${bundle}"></script>
-      </body>
-    </html>
-  `;
 };
