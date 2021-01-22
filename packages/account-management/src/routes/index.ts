b0bf7357a274c23477183416ccbad1e8ee8ad19a
rob.march <rob.march@digirati.com>
2021-01-22 15:33:46 +0000

    Ensure query params don't cause a whitelisted page to need unnecessary authentication

--- packages/account-management/src/routes/index.ts
+++ packages/account-management/src/routes/index.ts
@@ -5,7 +5,7 @@ const unAuthenticatedPages: string[] = ['/register', '/validated', '/error'];
 export const indexPage: RouteMiddleware = (context) => {
   const bundle = context.routes.url('assets-bundles');
 
-  const isUnauthenticatedPage: boolean = !!context.req.url && unAuthenticatedPages.includes(context.req.url);
+  const isUnauthenticatedPage: boolean = !!context.req.url && unAuthenticatedPages.includes(context.req.url.split('?')[0]);
   if (!isUnauthenticatedPage && !context.isAuthenticated()) {
     context.redirect('/login');
     return;
