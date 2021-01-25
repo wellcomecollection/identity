21aed2da7b20dabfcf628d94874951f6c06e2344
Alan Russell <alan.russell@digirati.com>
2021-01-25 15:40:59 +0000

    Move account details to cypress.json

--- packages/account-management/cypress/integration/login.ts
+++ packages/account-management/cypress/integration/login.ts
@@ -1,8 +1,8 @@
 describe('Login', () => {
   it('logs in successfully', () => {
     cy.visit('/login');
-    cy.get('input[name=username]').type('test');
-    cy.get('input[name=password]').type('test');
+    cy.get('input[name=username]').type(Cypress.env('username'));
+    cy.get('input[name=password]').type(Cypress.env('password'));
     cy.get('input[type=submit]').click();
     cy.url().should('include', '/app');
   });
