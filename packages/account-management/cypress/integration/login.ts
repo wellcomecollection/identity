edc572d93523cf8a64f2412e2950c3505b6b5e20
Alan Russell <alan.russell@digirati.com>
2021-01-25 15:54:35 +0000

    Get username and password straight from env file

--- packages/account-management/cypress/integration/login.ts
+++ packages/account-management/cypress/integration/login.ts
@@ -1,8 +1,8 @@
 describe('Login', () => {
   it('logs in successfully', () => {
     cy.visit('/login');
-    cy.get('input[name=username]').type(Cypress.env('username'));
-    cy.get('input[name=password]').type(Cypress.env('password'));
+    cy.get('input[name=username]').type(Cypress.env('TEST_USER'));
+    cy.get('input[name=password]').type(Cypress.env('TEST_PASSWORD'));
     cy.get('input[type=submit]').click();
     cy.url().should('include', '/app');
   });
--- packages/account-management/cypress/plugins/index.js
+++ packages/account-management/cypress/plugins/index.js
@@ -12,10 +12,18 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+const dotenvPlugin = require('cypress-dotenv');
+
 /**
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-}
+  config = dotenvPlugin(
+    config, // Cypress config object
+    undefined, // optional dotenv config object
+    true // Access all variables, not just ones prefixed CYPRESS_
+  );
+  return config;
+};
