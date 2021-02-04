e5d982b001f5b138329a7cf1e7139c16d2a34678
Daniel Grant <daniel.grant@digirati.com>
2021-02-04 09:16:08 +0000

    Removing redundant ECR repositories

--- infra/static/ecr.tf
+++ infra/static/ecr.tf
@@ -8,37 +8,3 @@ resource "aws_ecr_repository" "build" {
     }
   )
 }
-
-resource "aws_ecr_repository" "node" {
-  name = "uk.ac.wellcome/identity-node"
-
-  tags = merge(
-    local.common_tags,
-    {
-      "Name" = "uk.ac.wellcome/identity-node"
-    }
-  )
-}
-
-
-resource "aws_ecr_repository" "account_management_system" {
-  name = "identity-account-management-system"
-
-  tags = merge(
-    local.common_tags,
-    {
-      "Name" = "identity-account-management-system"
-    }
-  )
-}
-
-resource "aws_ecr_repository" "account_admin_system" {
-  name = "identity-account-admin-system"
-
-  tags = merge(
-    local.common_tags,
-    {
-      "Name" = "identity-account-admin-system"
-    }
-  )
-}
