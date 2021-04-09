56d9cdcd4374fe16b60cbce95fce1f1028ce0be3
Robert Kenny <kenoir@gmail.com>
2021-04-09 16:24:14 +0100

    Assume identity role in provider/backend

--- infra/scoped/backend.tf
+++ infra/scoped/backend.tf
@@ -2,9 +2,10 @@ terraform {
   required_version = "= 0.14.2" # Pin to a specific version to avoid accidental upgrading of the statefile
 
   backend "s3" {
-    bucket = "identity-remote-state"
-    key    = "terraform.tfstate"
-    region = "eu-west-1"
+    role_arn = "arn:aws:iam::770700576653:role/identity-developer"
+    bucket   = "identity-remote-state"
+    key      = "terraform.tfstate"
+    region   = "eu-west-1"
   }
 }
 
--- infra/scoped/provider.tf
+++ infra/scoped/provider.tf
@@ -1,10 +1,18 @@
 provider "aws" {
   region = "eu-west-1"
+
+  assume_role {
+    role_arn = "arn:aws:iam::770700576653:role/identity-developer"
+  }
 }
 
 provider "aws" {
   alias  = "aws_us-east-1"
   region = "us-east-1"
+
+  assume_role {
+    role_arn = "arn:aws:iam::770700576653:role/identity-developer"
+  }
 }
 
 provider "aws" {
