65dfe11339f40b26eb73009518ae7997d02b971e
jamie <j.parkinson@wellcome.ac.uk>
2021-05-12 18:27:25 +0100

    Inject Auth0 and redis dependencies into authorizer lambda handler

--- packages/apps/api-authorizer/src/index.ts
+++ packages/apps/api-authorizer/src/index.ts
@@ -6,126 +6,145 @@ import {
 } from 'aws-lambda';
 import { createNodeRedisClient, WrappedNodeRedisClient } from 'handy-redis';
 
-// Place these resources outside of the handler itself, so they can be reused between invocations.
+export const createLambdaHandler = (
+  auth0Client: Auth0Client,
+  redisClient: WrappedNodeRedisClient
+) => {
+  async function cacheLookup(
+    accessToken: string
+  ): Promise<Auth0UserInfo | null> {
+    return redisClient.get(accessToken).then((value) => {
+      if (value) {
+        console.debug(
+          `Cache hit for access token [${accessToken}] with value [${value}]`
+        );
+        return JSON.parse(value) as Auth0UserInfo;
+      } else {
+        console.debug(`Cache miss for access token [${accessToken}]`);
+        return null;
+      }
+    });
+  }
 
-const auth0Client: Auth0Client = new Auth0Client(
+  async function cacheInsert(
+    accessToken: string,
+    auth0UserInfo: Auth0UserInfo
+  ): Promise<string | null> {
+    const value: string = JSON.stringify(auth0UserInfo);
+    const ttl: number = Number(process.env.REDIS_CACHE_TTL!);
+    // 'EX' is the expiration (i.e. TTL) in seconds of the cache entry. We could also use 'PX' to provide the value in milliseconds.
+    return redisClient.set(accessToken, value, ['EX', ttl]).then((result) => {
+      console.debug(
+        `Cache put for access token [${accessToken}] with value [${value}]: [${result}]`
+      );
+      return value; // AFAICT this is always 'OK' for a successful operation
+    });
+  }
+
+  return async (
+    event: APIGatewayRequestAuthorizerEvent
+  ): Promise<APIGatewayAuthorizerResult> => {
+    if (!event.headers?.Authorization) {
+      console.debug('Authorization header is not present on request');
+      return buildAuthorizerResult('user', 'Deny', event.methodArn);
+    }
+
+    // Start by extracting the access token from the 'Authorization' header.
+    const authorizationHeader: string = event.headers.Authorization;
+    const accessToken: string | null = extractAccessToken(authorizationHeader);
+    if (!accessToken) {
+      console.debug(
+        `Authorization header [${authorizationHeader}] is not a valid bearer token`
+      );
+      return buildAuthorizerResult(
+        authorizationHeader,
+        'Deny',
+        event.methodArn
+      );
+    }
+
+    /*
+     * 1. Using the given access token, query the Redis cache for an existing entry.
+     * 2. If one is found, we assume the token is still valid and re-use the associated user information.
+     * 3. Otherwise, query the Auth0 API to test for validity and fetch the corresponding user information.
+     * 4. If the query was successful, store the user information in the Redis cache keyed against the access token.
+     *
+     * When the entry is inserted into the cache, it has a TTL defined by 'REDIS_CACHE_TTL'. Essentially, this value
+     * represents the maximum amount of time (in seconds) that a token can be expired / revoked by Auth0 before we detect
+     * that in this Lambda Function.
+     */
+    let auth0UserInfo: Auth0UserInfo | null = await cacheLookup(accessToken);
+    if (!auth0UserInfo) {
+      const auth0Validate: APIResponse<Auth0UserInfo> = await auth0Client.validateAccessToken(
+        accessToken
+      );
+      if (auth0Validate.status !== ResponseStatus.Success) {
+        if (auth0Validate.status === ResponseStatus.InvalidCredentials) {
+          console.debug(
+            `Access token [${accessToken}] rejected by Auth0: ${auth0Validate.message}`
+          );
+          return buildAuthorizerResult(accessToken, 'Deny', event.methodArn);
+        } else {
+          console.error(
+            `Unknown error processing access token [${accessToken}]: ${auth0Validate.message}`
+          );
+          return buildAuthorizerResult(accessToken, 'Deny', event.methodArn);
+        }
+      }
+      auth0UserInfo = auth0Validate.result;
+      await cacheInsert(accessToken, auth0UserInfo);
+    }
+
+    // At this point, we have a valid access token and have a handle on the caller user information. Now we determine if
+    // the user has access to the resource they are operating on.
+    if (
+      !validateRequest(
+        auth0UserInfo,
+        event.resource,
+        <ResourceAclMethod>event.httpMethod,
+        event.pathParameters
+      )
+    ) {
+      console.debug(
+        `Access token [${accessToken}] for user [${JSON.stringify(
+          auth0UserInfo
+        )}]` +
+          `cannot operate on [${event.httpMethod} ${event.resource}] with path parameters [${event.pathParameters}]`
+      );
+      return buildAuthorizerResult(
+        auth0UserInfo.userId,
+        'Deny',
+        event.methodArn
+      );
+    }
+
+    return buildAuthorizerResult(
+      auth0UserInfo.userId,
+      'Allow',
+      event.methodArn,
+      auth0UserInfo.userId,
+      isAdministrator(auth0UserInfo, {})
+    );
+  };
+};
+
+// Place these resources outside of the handler itself, so they can be reused between invocations.
+const lambdaAuth0Client: Auth0Client = new Auth0Client(
   process.env.AUTH0_API_ROOT!,
   process.env.AUTH0_API_AUDIENCE!,
   process.env.AUTH0_CLIENT_ID!,
   process.env.AUTH0_CLIENT_SECRET!
 );
 
-const redisClient: WrappedNodeRedisClient = createNodeRedisClient({
+const lambdaRedisClient: WrappedNodeRedisClient = createNodeRedisClient({
   host: process.env.REDIS_HOST!,
   port: Number(process.env.REDIS_PORT!),
 });
 
-export async function lambdaHandler(
-  event: APIGatewayRequestAuthorizerEvent
-): Promise<APIGatewayAuthorizerResult> {
-  if (!event.headers?.Authorization) {
-    console.debug('Authorization header is not present on request');
-    return buildAuthorizerResult('user', 'Deny', event.methodArn);
-  }
-
-  // Start by extracting the access token from the 'Authorization' header.
-  const authorizationHeader: string = event.headers.Authorization;
-  const accessToken: string | null = extractAccessToken(authorizationHeader);
-  if (!accessToken) {
-    console.debug(
-      `Authorization header [${authorizationHeader}] is not a valid bearer token`
-    );
-    return buildAuthorizerResult(authorizationHeader, 'Deny', event.methodArn);
-  }
-
-  /*
-   * 1. Using the given access token, query the Redis cache for an existing entry.
-   * 2. If one is found, we assume the token is still valid and re-use the associated user information.
-   * 3. Otherwise, query the Auth0 API to test for validity and fetch the corresponding user information.
-   * 4. If the query was successful, store the user information in the Redis cache keyed against the access token.
-   *
-   * When the entry is inserted into the cache, it has a TTL defined by 'REDIS_CACHE_TTL'. Essentially, this value
-   * represents the maximum amount of time (in seconds) that a token can be expired / revoked by Auth0 before we detect
-   * that in this Lambda Function.
-   */
-  let auth0UserInfo: Auth0UserInfo | null = await cacheLookup(accessToken);
-  if (!auth0UserInfo) {
-    const auth0Validate: APIResponse<Auth0UserInfo> = await auth0Client.validateAccessToken(
-      accessToken
-    );
-    if (auth0Validate.status !== ResponseStatus.Success) {
-      if (auth0Validate.status === ResponseStatus.InvalidCredentials) {
-        console.debug(
-          `Access token [${accessToken}] rejected by Auth0: ${auth0Validate.message}`
-        );
-        return buildAuthorizerResult(accessToken, 'Deny', event.methodArn);
-      } else {
-        console.error(
-          `Unknown error processing access token [${accessToken}]: ${auth0Validate.message}`
-        );
-        return buildAuthorizerResult(accessToken, 'Deny', event.methodArn);
-      }
-    }
-    auth0UserInfo = auth0Validate.result;
-    await cacheInsert(accessToken, auth0UserInfo);
-  }
-
-  // At this point, we have a valid access token and have a handle on the caller user information. Now we determine if
-  // the user has access to the resource they are operating on.
-  if (
-    !validateRequest(
-      auth0UserInfo,
-      event.resource,
-      <ResourceAclMethod>event.httpMethod,
-      event.pathParameters
-    )
-  ) {
-    console.debug(
-      `Access token [${accessToken}] for user [${JSON.stringify(
-        auth0UserInfo
-      )}]` +
-        `cannot operate on [${event.httpMethod} ${event.resource}] with path parameters [${event.pathParameters}]`
-    );
-    return buildAuthorizerResult(auth0UserInfo.userId, 'Deny', event.methodArn);
-  }
-
-  return buildAuthorizerResult(
-    auth0UserInfo.userId,
-    'Allow',
-    event.methodArn,
-    auth0UserInfo.userId,
-    isAdministrator(auth0UserInfo, {})
-  );
-}
-
-async function cacheLookup(accessToken: string): Promise<Auth0UserInfo | null> {
-  return redisClient.get(accessToken).then((value) => {
-    if (value) {
-      console.debug(
-        `Cache hit for access token [${accessToken}] with value [${value}]`
-      );
-      return JSON.parse(value) as Auth0UserInfo;
-    } else {
-      console.debug(`Cache miss for access token [${accessToken}]`);
-      return null;
-    }
-  });
-}
-
-async function cacheInsert(
-  accessToken: string,
-  auth0UserInfo: Auth0UserInfo
-): Promise<string | null> {
-  const value: string = JSON.stringify(auth0UserInfo);
-  const ttl: number = Number(process.env.REDIS_CACHE_TTL!);
-  // 'EX' is the expiration (i.e. TTL) in seconds of the cache entry. We could also use 'PX' to provide the value in milliseconds.
-  return redisClient.set(accessToken, value, ['EX', ttl]).then((result) => {
-    console.debug(
-      `Cache put for access token [${accessToken}] with value [${value}]: [${result}]`
-    );
-    return value; // AFAICT this is always 'OK' for a successful operation
-  });
-}
+export const lambdaHandler = createLambdaHandler(
+  lambdaAuth0Client,
+  lambdaRedisClient
+);
 
 function extractAccessToken(tokenString: string): null | string {
   const match = tokenString.match(/^Bearer (.*)$/);
