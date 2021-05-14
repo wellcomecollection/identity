07bf32c824a07cb6b0eb41edb965e6f9f9e467c3
jamie <j.parkinson@wellcome.ac.uk>
2021-05-14 13:12:53 +0100

    [wip] add requests service in ECS

--- infra/scoped/terraform.tf
+++ infra/scoped/terraform.tf
@@ -29,3 +29,27 @@ terraform {
     }
   }
 }
+
+data "terraform_remote_state" "infra_critical" {
+  backend = "s3"
+
+  config = {
+    role_arn = "arn:aws:iam::760097843905:role/platform-read_only"
+
+    bucket = "wellcomecollection-platform-infra"
+    key    = "terraform/platform-infrastructure/shared.tfstate"
+    region = "eu-west-1"
+  }
+}
+
+data "terraform_remote_state" "catalogue_api_shared" {
+  backend = "s3"
+
+  config = {
+    role_arn = "arn:aws:iam::756629837203:role/catalogue-read_only"
+
+    bucket = "wellcomecollection-catalogue-infra-delta"
+    key    = "terraform/catalogue/api/shared.tfstate"
+    region = "eu-west-1"
+  }
+}
