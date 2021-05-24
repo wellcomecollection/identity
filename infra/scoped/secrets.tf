3a9dd7d794b034e3dd4eb27f14dde1ae988f274b
Robert Kenny <kenoir@gmail.com>
2021-05-24 15:27:45 +0100

    add smoke tests to CI

--- infra/scoped/secrets.tf
+++ infra/scoped/secrets.tf
@@ -45,6 +45,7 @@ data "aws_secretsmanager_secret_version" "email_smtp_password_secret_version" {
 }
 
 # Account Management System
+# Create these secrets in the experience account
 
 resource "aws_secretsmanager_secret" "account_management_system-auth0_client_secret" {
   provider = aws.experience
@@ -71,8 +72,24 @@ resource "aws_secretsmanager_secret" "account_management_system-api_key" {
 }
 
 resource "aws_secretsmanager_secret_version" "account_management_system-api_key" {
-  provider      = aws.experience
+  provider = aws.experience
+
   secret_id     = aws_secretsmanager_secret.account_management_system-api_key.id
+  secret_string = aws_api_gateway_api_key.account_management_system.value
+}
+
+# Create these secrets in the identity account
+
+resource "aws_secretsmanager_secret" "account_management_system-api_key-identity" {
+  name = "identity/${terraform.workspace}/account_management_system/api_key"
+
+  tags = {
+    "Name" = "identity/${terraform.workspace}/account_management_system/api_key"
+  }
+}
+
+resource "aws_secretsmanager_secret_version" "account_management_system-api_key-identity" {
+  secret_id     = aws_secretsmanager_secret.account_management_system-api_key-identity.id
   secret_string = aws_api_gateway_api_key.account_management_system.value
 }
 
@@ -123,22 +140,38 @@ resource "aws_secretsmanager_secret" "es_credentials" {
 
 # Smoke Test
 
+# The test_user credentials are set manually in SM
+data "aws_secretsmanager_secret" "test_user_credentials" {
+  name = "identity/${terraform.workspace}/test_user/credentials"
+}
+
+data "aws_secretsmanager_secret_version" "test_user_credentials" {
+  secret_id = data.aws_secretsmanager_secret.test_user_credentials.id
+}
+
+# Setup a JSON secret to store
 locals {
+  decoded_test_user_credentials = jsondecode(
+    data.aws_secretsmanager_secret_version.test_user_credentials.secret_string
+  )
+
   smoke_test_credentials = {
+    username : local.decoded_test_user_credentials["username"]
+    password : local.decoded_test_user_credentials["password"]
     clientId : auth0_client.smoke_test.id
     clientSecret : auth0_client.smoke_test.client_secret
   }
 }
 
-resource "aws_secretsmanager_secret" "smoke_test-auth0_client_secret" {
-  name = "identity/${terraform.workspace}/smoke_test/auth0_client_secret"
+resource "aws_secretsmanager_secret" "smoke_test-auth0_credentials" {
+  name = "identity/${terraform.workspace}/smoke_test/credentials"
 
   tags = {
     "Name" = "identity/${terraform.workspace}/smoke_test/auth0_client_secret"
   }
 }
 
-resource "aws_secretsmanager_secret_version" "smoke_test-auth0_client_secret" {
-  secret_id     = aws_secretsmanager_secret.smoke_test-auth0_client_secret.id
+resource "aws_secretsmanager_secret_version" "smoke_test-auth0_credentials" {
+  secret_id     = aws_secretsmanager_secret.smoke_test-auth0_credentials.id
   secret_string = jsonencode(local.smoke_test_credentials)
 }
