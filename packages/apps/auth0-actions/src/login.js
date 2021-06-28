d97ff1725d5eab510b0e2c95b5b13c93f5c4f95c
jamie <j.parkinson@wellcome.ac.uk>
2021-06-28 11:07:39 +0100

    Use auth endpoint & async/await

--- packages/apps/auth0-actions/src/login.js
+++ packages/apps/auth0-actions/src/login.js
@@ -5,107 +5,80 @@ function login(email, password, callback) {
   const clientKey = configuration.CLIENT_KEY;
   const clientSecret = configuration.CLIENT_SECRET;
 
-  getPatronRecordByEmail(email)
-    .then((patronRecord) => {
-      validateCredentials(patronRecord.barcode, password)
-        .then(() => {
-          callback(null, {
-            user_id: 'p' + patronRecord.recordNumber,
-            email: patronRecord.email,
-            name: patronRecord.firstName + ' ' + patronRecord.lastName,
-            given_name: patronRecord.firstName,
-            family_name: patronRecord.lastName,
-          });
-        })
-        .catch((error) => {
-          callback(error);
-        });
-    })
-    .catch((error) => {
+  doLogin(email, password)
+    .then((result) => callback(null, result))
+    .catch((error) => callback(error));
+
+  async function doLogin(email, password) {
+    try {
+      const patronRecord = await getPatronRecordByEmail(email);
+      await validateCredentials(patronRecord.recordNumber, password);
+      return {
+        user_id: 'p' + patronRecord.recordNumber,
+        email: patronRecord.email,
+        name: patronRecord.firstName + ' ' + patronRecord.lastName,
+        given_name: patronRecord.firstName,
+        family_name: patronRecord.lastName,
+      };
+    } catch (error) {
       if (error.response && error.response.status === 404) {
-        callback(null);
-      } else {
-        callback(error);
+        return;
       }
-    });
+      throw error;
+    }
+  }
 
   // The /v6/patrons/validate expects passwords encoded using the encodeURIComponent scheme.
   // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/encodeURI
   // For example the password ;,/?:@&=+$-_.!~*'()# must be encoded as %3B%2C%2F%3F%3A%40%26%3D%2B%24-_.!~*%27()%23
-  async function validateCredentials(barcode, pin) {
-    return getInstance().then((instance) => {
-      return instance
-        .post(
-          '/v6/patrons/validate',
-          {
-            barcode: barcode,
-            pin: encodeURIComponent(pin),
-          },
-          {
-            validateStatus: (status) => status === 204,
-          }
-        )
-        .then(() => {
-          return getPatronRecordByBarcode(barcode);
-        });
-    });
-  }
-
-  async function getPatronRecordByBarcode(barcode) {
-    return getInstance().then((instance) => {
-      return instance
-        .get('/v6/patrons/find', {
-          params: {
-            varFieldTag: 'b',
-            varFieldContent: barcode,
-            fields: 'varFields',
-          },
-          validateStatus: (status) => status === 200,
-        })
-        .then((response) => {
-          return toPatronRecord(response.data);
-        });
-    });
+  async function validateCredentials(id, secret) {
+    const instance = await getInstance();
+    return instance.post(
+      '/v6/patrons/auth',
+      {
+        authMethod: 'native',
+        patronId: id,
+        patronSecret: secret,
+      },
+      {
+        validateStatus: (status) => status === 204,
+      }
+    );
   }
 
   async function getPatronRecordByEmail(email) {
-    return getInstance().then((instance) => {
-      return instance
-        .get('/v6/patrons/find', {
-          params: {
-            varFieldTag: 'z',
-            varFieldContent: email,
-            fields: 'varFields',
-          },
-          validateStatus: (status) => status === 200,
-        })
-        .then((response) => {
-          return toPatronRecord(response.data);
-        });
+    const instance = await getInstance();
+    const response = await instance.get('/v6/patrons/find', {
+      params: {
+        varFieldTag: 'z',
+        varFieldContent: email,
+        fields: 'varFields',
+      },
+      validateStatus: (status) => status === 200,
     });
+
+    return toPatronRecord(response.data);
   }
 
   async function getInstance() {
-    return axios
-      .post(
-        apiRoot + '/token',
-        {},
-        {
-          auth: {
-            username: clientKey,
-            password: clientSecret,
-          },
-          validateStatus: (status) => status === 200,
-        }
-      )
-      .then((response) => {
-        return axios.create({
-          baseURL: apiRoot,
-          headers: {
-            Authorization: 'Bearer ' + response.data.access_token,
-          },
-        });
-      });
+    const response = await axios.post(
+      apiRoot + '/token',
+      {},
+      {
+        auth: {
+          username: clientKey,
+          password: clientSecret,
+        },
+        validateStatus: (status) => status === 200,
+      }
+    );
+
+    return axios.create({
+      baseURL: apiRoot,
+      headers: {
+        Authorization: 'Bearer ' + response.data.access_token,
+      },
+    });
   }
 
   function toPatronRecord(data) {
