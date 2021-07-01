282d5578a2b45721a3c17903ae2dc99ced15db7d
jamie <j.parkinson@wellcome.ac.uk>
2021-07-01 11:45:18 +0100

    Add a comment about the error class

--- packages/apps/auth0-actions/src/databaseScriptGlobals.d.ts
+++ packages/apps/auth0-actions/src/databaseScriptGlobals.d.ts
@@ -1,6 +1,8 @@
 export {};
 
 declare global {
+  // Some custom errors are available:
+  // https://auth0.com/docs/connections/database/custom-db/error-handling#types-of-errors
   class WrongUsernameOrPasswordError extends Error {
     constructor(emailOrId?: string, message?: string);
   }
