84f3d0a6d72575066f7daca1425512f40356eca9
jamie <j.parkinson@wellcome.ac.uk>
2021-09-02 18:24:32 +0100

    Add tests for /user/{userId}/lock

--- packages/apps/api/test/routes/users_user_id_lock.test.ts
+++ packages/apps/api/test/routes/users_user_id_lock.test.ts
@@ -1,11 +1,45 @@
+import { ExistingUser, mockedApi } from './fixtures/mockedApi';
+
+const testUser: ExistingUser = {
+  userId: 12345678,
+  email: 'test@test.com',
+  firstName: 'Test',
+  lastName: 'User',
+};
+
 describe('/users/{userId}/lock', () => {
   describe('PUT /users/{userId}/lock', () => {
-    it('locks a user in Auth0', () => {});
-    it('404s for users that do not exist', () => {});
+    it('locks a user in Auth0', async () => {
+      const { clients, api } = mockedApi([testUser]);
+      const response = await api.put(`/users/${testUser.userId}/lock`);
+      expect(response.statusCode).toBe(200);
+
+      const updatedUser = clients.auth0.get(testUser.userId);
+      expect(updatedUser?.locked).toBe(true);
+    });
+
+    it('404s for users that do not exist', async () => {
+      const { api } = mockedApi([testUser]);
+      const response = await api.put(`/users/66666666/lock`);
+      expect(response.statusCode).toBe(404);
+    });
   });
 
   describe('DELETE /users/{userId}/lock', () => {
-    it('unlocks a user in Auth0', () => {});
-    it('404s for users that do not exist', () => {});
+    it('unlocks a user in Auth0', async () => {
+      const { clients, api } = mockedApi([testUser]);
+      await api.put(`/users/${testUser.userId}/lock`);
+      const response = await api.delete(`/users/${testUser.userId}/lock`);
+      expect(response.statusCode).toBe(204);
+
+      const updatedUser = clients.auth0.get(testUser.userId);
+      expect(updatedUser?.locked).toBe(false);
+    });
+
+    it('404s for users that do not exist', async () => {
+      const { api } = mockedApi([testUser]);
+      const response = await api.delete(`/users/66666666/lock`);
+      expect(response.statusCode).toBe(404);
+    });
   });
 });
