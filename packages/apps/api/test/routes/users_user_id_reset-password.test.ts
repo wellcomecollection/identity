5d8011b16efbfc2d03b25af3fc04ff7cfaa3679c
jamie <j.parkinson@wellcome.ac.uk>
2021-09-03 09:38:19 +0100

    Add tests for /user/{userId}/reset-password

--- packages/apps/api/test/routes/users_user_id_reset-password.test.ts
+++ packages/apps/api/test/routes/users_user_id_reset-password.test.ts
@@ -1,6 +1,30 @@
+import { ExistingUser, mockedApi } from './fixtures/mockedApi';
+
+const testUser: ExistingUser = {
+  userId: 12345678,
+  email: 'test@test.com',
+  firstName: 'Test',
+  lastName: 'User',
+};
+
 describe('/users/{userId}/reset-password', () => {
   describe('PUT /users/{userId}/reset-password', () => {
-    it('sends a password reset email', () => {});
-    it('404s for users that do not exist', () => {});
+    it('sends a password reset email', async () => {
+      const { api, clients } = mockedApi([testUser]);
+      const response = await api.put(
+        `/users/${testUser.userId}/reset-password`
+      );
+
+      expect(response.statusCode).toBe(200);
+      expect(clients.auth0.sendPasswordResetEmail).toHaveBeenCalledWith(
+        testUser.email
+      );
+    });
+
+    it('404s for users that do not exist', async () => {
+      const { api } = mockedApi([testUser]);
+      const response = await api.put(`/users/6666666/reset-password`);
+      expect(response.statusCode).toBe(404);
+    });
   });
 });
