baff414ab63dddc6c97b0521f5508ff263cb1555
jamie <j.parkinson@wellcome.ac.uk>
2021-11-24 13:27:51 +0000

    Fix name of verify script

--- packages/apps/auth0-actions/src/verify.ts
+++ packages/apps/auth0-actions/src/verify.ts
@@ -1,9 +1,9 @@
 import { callbackify } from 'util';
 import { User } from 'auth0';
 
-async function changeEmail(email: string): Promise<User> {
+async function verify(email: string): Promise<User> {
   // Do some things
   return {};
 }
 
-export default callbackify(changeEmail);
+export default callbackify(verify);
