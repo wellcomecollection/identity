2f82ad306061812ad563ccfc30a0ae71ce5e681d
jamie <j.parkinson@wellcome.ac.uk>
2021-11-25 17:38:54 +0000

    Add resource server

---
+++ infra/scoped/auth0-resource-server.tf
@@ -0,0 +1,41 @@
+// A resource server in OAuth2 parlance is an "API" in Auth0 parlance
+resource "auth0_resource_server" "identity_api" {
+  name        = "Identity API"
+  identifier  = local.identity_v1_endpoint
+  signing_alg = "RS256"
+
+  // These are, by convention, permissions related to the user holding the token only
+  // ie they do not mean "read any patron's info"
+  // https://auth0.com/docs/configure/apis/add-api-permissions
+  // Keep in alphabetical order to stop meaningless tf state changes
+  scopes {
+    value       = "create:requests"
+    description = "Create item requests"
+  }
+
+  scopes {
+    value       = "delete:patron"
+    description = "Request deletion"
+  }
+
+  scopes {
+    value       = "read:patron"
+    description = "Read patron info"
+  }
+
+  scopes {
+    value       = "read:requests"
+    description = "Read item requests"
+  }
+
+  scopes {
+    value       = "update:email"
+    description = "Update email"
+  }
+
+  // Applications are allowed refresh tokens
+  allow_offline_access                            = true
+  token_lifetime                                  = 60 * 60 // 1 hour
+  token_lifetime_for_web                          = 60 * 60 // 1 hour
+  skip_consent_for_verifiable_first_party_clients = true
+}
