fecbc5205333601b8e23559e597cde2702485f00
jamie <j.parkinson@wellcome.ac.uk>
2021-11-25 18:09:34 +0000

    It is not necessary to check whether a user exists before changing email

--- packages/apps/auth0-actions/jest.setup.js
+++ packages/apps/auth0-actions/jest.setup.js
@@ -12,11 +12,5 @@ class WrongUsernameOrPasswordError extends Error {
     super(message);
   }
 }
-class ValidationError extends Error {
-  constructor(errorId, message) {
-    super(message);
-  }
-}
 
 global.WrongUsernameOrPasswordError = WrongUsernameOrPasswordError;
-global.ValidationError = ValidationError;
