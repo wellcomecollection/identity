8bce1af8ea5493cc9f669da77877e084781be695
jamie <j.parkinson@wellcome.ac.uk>
2021-12-09 16:08:06 +0000

    Only add custom claim to ID token, make it opt-in

--- packages/apps/auth0-actions/src/add_custom_claims.ts
+++ packages/apps/auth0-actions/src/add_custom_claims.ts
@@ -4,15 +4,24 @@ import { Event, API } from './types/post-login';
 // Custom claims MUST be namespaced with a URL as per the OIDC spec
 // https://auth0.com/docs/security/tokens/json-web-tokens/create-namespaced-custom-claims
 const CLAIM_NAMESPACE = 'https://wellcomecollection.org';
-const BARCODE_CLAIM = `${CLAIM_NAMESPACE}/patron_barcode`;
+
+// Rather than always including our custom claims on tokens, we make them requestable
+// by a namespaced scope (ie `weco:<claim>`)
+const SCOPE_NAMESPACE = 'weco';
+
+const barcodeName = 'patron_barcode';
+const barcodeClaim = `${CLAIM_NAMESPACE}/${barcodeName}`;
+const barcodeScope = `${SCOPE_NAMESPACE}:${barcodeName}`;
+
+const hasScope = <T>(event: Event<T>, scope: string): boolean =>
+  !!event.transaction?.requested_scopes?.includes(scope);
 
 export const onExecutePostLogin = async (
   event: Event<Auth0User>,
   api: API<Auth0User>
 ) => {
   const barcode = event.user.app_metadata?.barcode;
-  if (barcode) {
-    api.accessToken.setCustomClaim(BARCODE_CLAIM, barcode);
-    api.idToken.setCustomClaim(BARCODE_CLAIM, barcode);
+  if (barcode && hasScope(event, barcodeScope)) {
+    api.idToken.setCustomClaim(barcodeClaim, barcode);
   }
 };
--- packages/apps/auth0-actions/tests/add_custom_claims.test.ts
+++ packages/apps/auth0-actions/tests/add_custom_claims.test.ts
@@ -1,5 +1,5 @@
 import { onExecutePostLogin } from '../src/add_custom_claims';
-import { API, Event } from '../src/types/post-login';
+import { API, Event, EventTransaction } from '../src/types/post-login';
 import { Auth0User } from '@weco/auth0-client';
 
 describe('add_custom_claims', () => {
@@ -7,20 +7,17 @@ describe('add_custom_claims', () => {
   const user: Auth0User = {
     app_metadata: { barcode },
   } as Auth0User;
-  const mockEvent: Event<Auth0User> = { user } as Event<Auth0User>;
+  const createEvent = (user: Auth0User, scopes: string[]): Event<Auth0User> =>
+    ({
+      user,
+      transaction: { requested_scopes: scopes } as EventTransaction,
+    } as Event<Auth0User>);
 
-  it('adds the patron barcode to the access token', () => {
-    onExecutePostLogin(mockEvent, mockPostLoginApi);
-    expect(
-      mockPostLoginApi.accessToken.setCustomClaim
-    ).toHaveBeenLastCalledWith(
-      'https://wellcomecollection.org/patron_barcode',
-      barcode
+  it('adds the patron barcode to the ID token if the barcode scope is present', () => {
+    onExecutePostLogin(
+      createEvent(user, ['weco:patron_barcode']),
+      mockPostLoginApi
     );
-  });
-
-  it('adds the patron barcode to the ID token', () => {
-    onExecutePostLogin(mockEvent, mockPostLoginApi);
     expect(mockPostLoginApi.idToken.setCustomClaim).toHaveBeenLastCalledWith(
       'https://wellcomecollection.org/patron_barcode',
       barcode
@@ -30,9 +27,16 @@ describe('add_custom_claims', () => {
   it('does not add anything if no patron barcode is present', () => {
     jest.clearAllMocks();
     onExecutePostLogin(
-      { user: { app_metadata: {} } as Auth0User } as Event<Auth0User>,
+      createEvent({ app_metadata: {} } as Auth0User, ['weco:patron_barcode']),
       mockPostLoginApi
     );
+    expect(mockPostLoginApi.accessToken.setCustomClaim).not.toBeCalled();
+    expect(mockPostLoginApi.idToken.setCustomClaim).not.toBeCalled();
+  });
+
+  it('does not add anything if the scope is not present', () => {
+    jest.clearAllMocks();
+    onExecutePostLogin(createEvent(user, []), mockPostLoginApi);
     expect(mockPostLoginApi.accessToken.setCustomClaim).not.toBeCalled();
     expect(mockPostLoginApi.idToken.setCustomClaim).not.toBeCalled();
   });
