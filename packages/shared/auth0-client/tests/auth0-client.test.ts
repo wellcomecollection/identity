7973f0e266c928cca7ae3bdc487d03a4130690ef
jamie <j.parkinson@wellcome.ac.uk>
2021-12-21 10:14:53 +0000

    Remove duplicate test data

--- packages/shared/auth0-client/tests/auth0-client.test.ts
+++ packages/shared/auth0-client/tests/auth0-client.test.ts
@@ -3,10 +3,10 @@ import {
   ResponseStatus,
   SuccessResponse,
 } from '@weco/identity-common';
-import { SierraUserIdPrefix, SierraConnection } from '../src/auth0';
 import { Auth0User, HttpAuth0Client } from '../src';
 import mockAuth0Server, { apiRoot, routeUrls } from './mock-auth0-server';
 import { compose, rest } from 'msw';
+import { email, testUser, userId } from './test-user';
 
 describe('HTTP Auth0 client', () => {
   const apiAudience: string = 'http://my-api.localhost';
@@ -32,7 +32,7 @@ describe('HTTP Auth0 client', () => {
       expect(response.status).toBe(ResponseStatus.Success);
 
       const result = (<SuccessResponse<Auth0User>>response).result;
-      expect(result).toEqual(expect.objectContaining(user));
+      expect(result).toEqual(expect.objectContaining(testUser));
     });
 
     it('returns a NotFound if the user does not exist', async () => {
@@ -64,7 +64,7 @@ describe('HTTP Auth0 client', () => {
       const result = (<SuccessResponse<Auth0User>>response).result;
       expect(result).toEqual(
         expect.objectContaining({
-          ...user,
+          ...testUser,
           email: newEmail,
         })
       );
@@ -113,44 +113,3 @@ describe('HTTP Auth0 client', () => {
     });
   });
 });
-
-const userId: number = 123456;
-const firstName: string = 'Test';
-const lastName: string = 'User';
-const name: string = firstName + ' ' + lastName;
-const email: string = 'test.user@example.com';
-const password: string = 'superstrongpassword';
-const picture: string = 'https://i1.wp.com/cdn.auth0.com/avatars/tu.png?ssl=1';
-const creationDate: string = '2020-11-18T14:27:34.766Z';
-const updatedDate: string = '2020-12-09T09:09:24.042Z';
-const passwordResetDate: string = '2020-11-19T12:03:02.999Z';
-const lastLoginDate: string = '2020-12-09T09:09:24.042Z';
-const lastLoginIp: string = '127.0.0.1';
-const totalLogins: number = 10;
-const emailValidated: boolean = true;
-const locked: boolean = false;
-
-const user: Auth0User = {
-  created_at: creationDate,
-  email: email,
-  identities: [
-    {
-      user_id: 'p' + userId,
-      provider: 'auth0',
-      connection: SierraConnection,
-      isSocial: false,
-    },
-  ],
-  name: name,
-  given_name: firstName,
-  family_name: lastName,
-  nickname: email.substring(0, email.lastIndexOf('@')),
-  picture: picture,
-  updated_at: updatedDate,
-  user_id: SierraUserIdPrefix + userId,
-  last_password_reset: passwordResetDate,
-  email_verified: emailValidated,
-  last_ip: lastLoginIp,
-  last_login: lastLoginDate,
-  logins_count: totalLogins,
-};
--- packages/shared/auth0-client/tests/test-user.ts
+++ packages/shared/auth0-client/tests/test-user.ts
@@ -1,21 +1,22 @@
 import { Auth0User } from '../src';
 import { SierraConnection, SierraUserIdPrefix } from '../src/auth0';
 
-const userId: number = 123456;
-const firstName: string = 'Test';
-const lastName: string = 'User';
-const name: string = firstName + ' ' + lastName;
-const email: string = 'test.user@example.com';
-const password: string = 'superstrongpassword';
-const picture: string = 'https://i1.wp.com/cdn.auth0.com/avatars/tu.png?ssl=1';
-const creationDate: string = '2020-11-18T14:27:34.766Z';
-const updatedDate: string = '2020-12-09T09:09:24.042Z';
-const passwordResetDate: string = '2020-11-19T12:03:02.999Z';
-const lastLoginDate: string = '2020-12-09T09:09:24.042Z';
-const lastLoginIp: string = '127.0.0.1';
-const totalLogins: number = 10;
-const emailValidated: boolean = true;
-const locked: boolean = false;
+export const userId: number = 123456;
+export const firstName: string = 'Test';
+export const lastName: string = 'User';
+export const name: string = firstName + ' ' + lastName;
+export const email: string = 'test.user@example.com';
+export const password: string = 'superstrongpassword';
+export const picture: string =
+  'https://i1.wp.com/cdn.auth0.com/avatars/tu.png?ssl=1';
+export const creationDate: string = '2020-11-18T14:27:34.766Z';
+export const updatedDate: string = '2020-12-09T09:09:24.042Z';
+export const passwordResetDate: string = '2020-11-19T12:03:02.999Z';
+export const lastLoginDate: string = '2020-12-09T09:09:24.042Z';
+export const lastLoginIp: string = '127.0.0.1';
+export const totalLogins: number = 10;
+export const emailValidated: boolean = true;
+export const locked: boolean = false;
 
 export const testUser: Auth0User = {
   created_at: creationDate,
