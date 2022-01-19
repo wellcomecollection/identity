e3d01d9c04e8e500b4547f1d2025b8d73c8c6c65
jamie <j.parkinson@wellcome.ac.uk>
2022-01-19 12:13:16 +0000

    Store emails in notes

--- packages/shared/sierra-client/tests/notes.test.ts
+++ packages/shared/sierra-client/tests/notes.test.ts
@@ -4,21 +4,30 @@ describe('notes field contents', () => {
   describe('parsing', () => {
     it('parses a string that adheres to our format', () => {
       const field =
-        'Auth0: assumed to be verified at login 2022-02-22T00:00:00.000Z';
+        'Auth0: email@example.com assumed to be verified at login 2022-02-22T00:00:00.000Z';
       const result = getAuth0Note(field);
 
-      expect(result).toEqual(['AssumedVerified', new Date('2022-02-22')]);
+      expect(result).toEqual({
+        type: 'EmailAssumedVerified',
+        payload: 'email@example.com',
+        date: new Date('2022-02-22'),
+      });
     });
 
     it('ignores malformed strings', () => {
       const malformed1 =
-        'Auth1: assumed to be verified at login 2022-02-22T00:00:00.000Z';
-      const malformed2 = 'Auth0: arble garble blarble 2022-02-22T00:00:00.000Z';
-      const malformed3 = 'Auth0: verified about a week ago, I think';
+        'Auth1: email@example.com assumed to be verified at login 2022-02-22T00:00:00.000Z';
+      const malformed2 =
+        'Auth0: email@example.com arble garble blarble 2022-02-22T00:00:00.000Z';
+      const malformed3 =
+        'Auth0: email@example.com verified about a week ago, I think';
+      const malformed4 =
+        'Auth0: invalid@email verified 2022-02-22T00:00:00.000Z';
 
       expect(getAuth0Note(malformed1)).toBe(undefined);
       expect(getAuth0Note(malformed2)).toBe(undefined);
       expect(getAuth0Note(malformed3)).toBe(undefined);
+      expect(getAuth0Note(malformed4)).toBe(undefined);
     });
 
     it('ignores irrelevant strings', () => {
@@ -30,11 +39,23 @@ describe('notes field contents', () => {
   describe('creation', () => {
     it('creates a note field for a given state', () => {
       const date = new Date('2022-02-22');
-      expect(createAuth0Note('Verified', date)).toBe(
-        'Auth0: verified 2022-02-22T00:00:00.000Z'
-      );
-      expect(createAuth0Note('AssumedVerified', date)).toBe(
-        'Auth0: assumed to be verified at login 2022-02-22T00:00:00.000Z'
+
+      expect(
+        createAuth0Note({
+          type: 'EmailVerified',
+          payload: 'email@example.com',
+          date,
+        })
+      ).toBe('Auth0: email@example.com verified 2022-02-22T00:00:00.000Z');
+
+      expect(
+        createAuth0Note({
+          type: 'EmailAssumedVerified',
+          payload: 'email@example.com',
+          date,
+        })
+      ).toBe(
+        'Auth0: email@example.com assumed to be verified at login 2022-02-22T00:00:00.000Z'
       );
     });
   });
