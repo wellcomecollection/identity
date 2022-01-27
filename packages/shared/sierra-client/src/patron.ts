55bbbaca6b24331db43692f56e7a3bebdbd4f983
jamie <j.parkinson@wellcome.ac.uk>
2022-01-27 09:40:55 +0000

    Fix types in the patron tests

--- packages/shared/sierra-client/src/patron.ts
+++ packages/shared/sierra-client/src/patron.ts
@@ -136,6 +136,7 @@ export const varFieldTags = {
   email: 'z',
   notes: 'x',
   name: 'n',
+  message: 'm',
 } as const;
 
 type VarFieldTag = typeof varFieldTags[keyof typeof varFieldTags];
@@ -182,7 +183,10 @@ export type PatronCreate = {
 
 export type VarField = {
   fieldTag: VarFieldTag;
+  marcTag?: string;
   content?: string;
+  ind1?: string;
+  ind2?: string;
   subfields?: SubField[];
 };
 
