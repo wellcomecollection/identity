cf5316071a91fbae63fafbb89e7b0d9837281080
Donald Gray <donald.gray@digirati.com>
2022-04-14 11:40:51 +0100

    Update custom claims to support roles

--- packages/apps/auth0-actions/tests/add_custom_claims.test.ts
+++ packages/apps/auth0-actions/tests/add_custom_claims.test.ts
@@ -4,8 +4,9 @@ import { Auth0User } from '@weco/auth0-client';
 
 describe('add_custom_claims', () => {
   const barcode = '1234567';
+  const role = 'Reader';
   const user: Auth0User = {
-    app_metadata: { barcode, role: 'Reader' },
+    app_metadata: { barcode, role },
   } as Auth0User;
   const createEvent = (user: Auth0User, scopes: string[]): Event<Auth0User> =>
     ({
@@ -33,6 +34,43 @@ describe('add_custom_claims', () => {
     expect(mockPostLoginApi.accessToken.setCustomClaim).not.toBeCalled();
     expect(mockPostLoginApi.idToken.setCustomClaim).not.toBeCalled();
   });
+
+  it('adds the patron role to the ID token if the role scope is present', () => {
+    onExecutePostLogin(
+      createEvent(user, ['weco:patron_role']),
+      mockPostLoginApi
+    );
+    expect(mockPostLoginApi.idToken.setCustomClaim).toHaveBeenLastCalledWith(
+      'https://wellcomecollection.org/patron_role',
+      role
+    );
+  });
+
+  it('does not add anything if no patron role is present', () => {
+    jest.clearAllMocks();
+    onExecutePostLogin(
+      createEvent({ app_metadata: {} } as Auth0User, ['weco:patron_role']),
+      mockPostLoginApi
+    );
+    expect(mockPostLoginApi.accessToken.setCustomClaim).not.toBeCalled();
+    expect(mockPostLoginApi.idToken.setCustomClaim).not.toBeCalled();
+  });
+
+  it('adds the patron role and barcode to the ID token if boths scopes are present', () => {
+    onExecutePostLogin(
+      createEvent(user, ['weco:patron_role', 'weco:patron_barcode']),
+      mockPostLoginApi
+    );
+    expect(mockPostLoginApi.idToken.setCustomClaim).toHaveBeenCalledWith(
+      'https://wellcomecollection.org/patron_role',
+      role
+    );
+    expect(mockPostLoginApi.idToken.setCustomClaim).toHaveBeenCalledWith(
+      'https://wellcomecollection.org/patron_barcode',
+      barcode
+    );
+  });
+
 
   it('does not add anything if the scope is not present', () => {
     jest.clearAllMocks();
