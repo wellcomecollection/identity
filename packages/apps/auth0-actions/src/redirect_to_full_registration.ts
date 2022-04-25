e4fe97acb3616989a56e634f4f331ce80f549ae3
melanierogan <roganmc@tcd.ie>
2022-04-25 14:49:27 +0100

    adding sierraClient to eventually call sierra to create patron

--- packages/apps/auth0-actions/src/redirect_to_full_registration.ts
+++ packages/apps/auth0-actions/src/redirect_to_full_registration.ts
@@ -1,5 +1,15 @@
 import { Auth0User } from '@weco/auth0-client';
 import { Event, API } from './types/post-login';
+import { HttpSierraClient } from '@weco/sierra-client';
+import { ResponseStatus } from '@weco/identity-common';
+import { callbackify } from 'util';
+
+declare const configuration: {
+  API_ROOT: string;
+  CLIENT_KEY: string;
+  CLIENT_SECRET: string;
+};
+
 
 const REGISTRATION_FORM_URL = 'http://localhost:3000/account/registration';
 export const onExecutePostLogin = async (
@@ -39,5 +49,12 @@ export const onContinuePostLogin = async (
     secret: event.secrets.AUTH0_ACTION_SECRET,
   });
 
+  const apiRoot = configuration.API_ROOT;
+  const clientKey = configuration.CLIENT_KEY;
+  const clientSecret = configuration.CLIENT_SECRET;
+  
+
   // TODO: take the payload firstName, Surname and termsandconditions and pass to Sierra to create a patron
+  const sierraClient = new HttpSierraClient(apiRoot, clientKey, clientSecret);
+  // const createPatron = await sierraClient.createPatron();
 };
