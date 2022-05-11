219fe3663bbc71fc90c0cf8eca01f3ade66f0511
melanierogan <roganmc@tcd.ie>
2022-05-11 14:47:12 +0100

    further update comments

--- packages/apps/auth0-actions/src/redirect_to_full_registration.ts
+++ packages/apps/auth0-actions/src/redirect_to_full_registration.ts
@@ -1,6 +1,5 @@
 import { Auth0User } from '@weco/auth0-client';
 import { Event, API } from './types/post-login';
-import { callbackify } from 'util';
 
 
 const REGISTRATION_FORM_URL = 'http://localhost:3000/account/registration';
@@ -11,7 +10,7 @@ export const onExecutePostLogin = async (
   api: API<Auth0User>
 ) => {
 
-  // If the user has accepted the terms already, we don't need to do anything else
+  // If the user has accepted the terms already, we don't need to do anything else so bail out here
   if (event.user.app_metadata.terms_and_conditions_accepted) return;
 
   // We send the user straight to the full registration form after they first register email and password
@@ -21,12 +20,12 @@ export const onExecutePostLogin = async (
 
   // TODO: Confirm if we need to add firstname and lastname or if endpoint does that for us and we only need to redirect
   // to success page on successful update of user in auth0 and sierra
-  
+
   // the auth0 action continues below and adds firstname, username and if terms are accepted
   // auth0 action will then redirect to success page
 
   const sessionToken = api.redirect.encodeToken({
-    // this token must match the one used in Idenity App (Weco)
+    // this token must match the one used in Idenity API
     secret: event.secrets.AUTH0_ACTION_SECRET,
     payload: {
       iss: `https://${event.request.hostname}/`,
@@ -45,12 +44,17 @@ export const onExecutePostLogin = async (
 // Handler that will be invoked when this action is resuming after an external redirect. If your
 // onExecutePostLogin function does not perform a redirect, this function can be safely ignored.
 
+
 export const onContinuePostLogin = async (
   event: Event<Auth0User>,
   api: API<Auth0User>
 ) => {
   // Once the full registration form has been submitted, it is signed with JWT handler on Identity App (Weco)
   // This jwt token is sent back to /continue on auth0 actions and we validate/decode the token to get formData
+  // On success of full registration form submission at Identity API updateUserAfterRegistration endpoint 
+  // we redirect to /continue with encodes formData from the endpoint
+  // any data we send to /continue is tokenised and validated below
+
   const payload = api.redirect.validateToken({
     secret: event.secrets.AUTH0_ACTION_SECRET,
     tokenParameterName: 'token',
