c4a82ea9b424078c4bfbc4e7e22cd9283fe5983f
melanierogan <roganmc@tcd.ie>
2022-05-16 16:29:47 +0100

    getting tf ready to deploy new redirect_to_full_registration action

--- infra/scoped/auth0-actions.tf
+++ infra/scoped/auth0-actions.tf
@@ -13,11 +13,36 @@ resource "auth0_action" "add_custom_claims" {
   }
 }
 
+resource "auth0_action" "redirect_to_full_registration" {
+  name   = "Add Custom Claims"
+  code   = file("${path.module}/data/empty.js")
+  deploy = true
+
+  supported_triggers {
+    id      = "post-login"
+    version = "v2"
+  }
+
+  lifecycle {
+    ignore_changes = [code]
+  }
+
+  secrets {
+    name:
+    value: 
+  }
+}
+
 resource "auth0_trigger_binding" "post_login" {
   trigger = "post-login"
 
   actions {
     id           = auth0_action.add_custom_claims.id
     display_name = auth0_action.add_custom_claims.name
+  }
+
+    actions {
+    id           = auth0_action.redirect_to_full_registration.id
+    display_name = auth0_action.redirect_to_full_registration.name
   }
 }
