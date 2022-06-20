4e7959a6c51326adab9e32118260c09eeae867a2
melanierogan <roganmc@tcd.ie>
2022-06-20 14:52:05 +0100

    share the redirect_action_secret with experience account for weco frontend

--- infra/scoped/secrets.tf
+++ infra/scoped/secrets.tf
@@ -87,6 +87,7 @@ module "secrets_experience" {
   key_value_map = {
     "identity/${terraform.workspace}/account_management_system/auth0_client_secret" = auth0_client.account_management_system.client_secret
     "identity/${terraform.workspace}/account_management_system/api_key"             = aws_api_gateway_api_key.account_management_system.value
+    "identity/${terraform.workspace}/redirect_action_secret"                        = data.aws_secretsmanager_secret_version.redirect_action_secret.secret_string
   }
 
   providers = {
