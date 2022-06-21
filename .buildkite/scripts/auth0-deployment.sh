d5d3d20accbcaf26774f34ada913f7572b0879f8
Alex Chan <alex@alexwlchan.net>
2022-06-21 17:44:58 +0100

    Toss in the Git commit also

--- .buildkite/scripts/auth0-deployment.sh
+++ .buildkite/scripts/auth0-deployment.sh
@@ -9,6 +9,14 @@ AUTH0_ACTIONS_FILE="/app/.buildkite/build/auth0-actions.json"
 DATABASE_SCRIPTS="get_user login change_password change_email verify create delete"
 ACTIONS="add_custom_claims redirect_to_full_registration"
 
+# We add this header to the Auth0 scripts to make it easier to debug
+# what version of the code is actually running.
+#
+# We've had issues in the past where it wasn't obvious if changes were
+# actually being deployed.
+CURRENT_COMMIT=$(git rev-parse HEAD)
+DEBUG_HEADER="// Set by $0 at $(date) (commit $CURRENT_COMMIT)"
+
 # Use --env=true with a0deploy as per
 # https://github.com/auth0/auth0-deploy-cli/issues/544
 
@@ -17,21 +25,15 @@ function __do_deployment() {
   a0deploy --env=true export --format directory --output_folder "${AUTH0_EXPORT_DIR}/"
 
   # Deploy database scripts
-  #
-  # We prepend the date when the script was written to help debug issues
-  # with the deployment.
   for script in ${DATABASE_SCRIPTS}
   do
     dest_script="${AUTH0_EXPORT_DIR}/database-connections/${AUTH0_CONNECTION_NAME}/${script}.js"
 
     cp -v "${SCRIPTS_BUILD_DIR}/${script}.js" "$dest_script"
-    echo -e "// Set by $0 at $(date)\n\n$(cat "$dest_script")" > "$dest_script"
+    echo -e "$DEBUG_HEADER\n\n$(cat "$dest_script")" > "$dest_script"
   done
 
   # Deploy actions
-  #
-  # We prepend the date when the script was written to help debug issues
-  # with the deployment.
   for action in ${ACTIONS}
   do
     action_name=$(jq -r ".names.${action}" ${AUTH0_ACTIONS_FILE})
@@ -39,7 +41,7 @@ function __do_deployment() {
     dest_script="${AUTH0_EXPORT_DIR}/actions/${action_name}/code.js"
 
     cp -v "${ACTIONS_BUILD_DIR}/${action}.js" "$dest_script"
-    echo -e "// Set by $0 at $(date)\n\n$(cat "$dest_script")" > "$dest_script"
+    echo -e "$DEBUG_HEADER\n\n$(cat "$dest_script")" > "$dest_script"
   done
 
   a0deploy --env=true import --input_file "${AUTH0_EXPORT_DIR}/"
