b01227155a0f031e6a66ebaacf0b36c75ad047a2
jamie <j.parkinson@wellcome.ac.uk>
2022-06-24 12:44:24 +0100

    Need to install lint-staged first

--- .buildkite/scripts/linting.sh
+++ .buildkite/scripts/linting.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-yarn lint-staged --diff="main..$BUILDKITE_BRANCH"
+yarn global add lint-staged prettier
+lint-staged --diff="main..$BUILDKITE_BRANCH"
 
 # Fail if any changes were made by linter
 [[ -n $(git status -s) ]]
