c90e31dd52d5894f7636ae166dc0a0c5a892c338
Alex Chan <alex@alexwlchan.net>
2022-06-27 08:45:18 +0100

    Remove a bunch of temporary/debugging code

--- packages/apps/api/test/routes/users_user_id_registration.test.ts
+++ packages/apps/api/test/routes/users_user_id_registration.test.ts
@@ -10,26 +10,17 @@ describe('/users/{userId}/registration', () => {
       const testUser = randomExistingUser({ firstName: 'Auth0_Registration_undefined', lastName: 'Auth0_Registration_tempLastName' });
       const { api, clients } = mockedApi([testUser]);
 
-      console.log('@@AWLC 1');
-
       const response = await withCallerId('@machine')(
         api
           .put(`/users/${testUser.userId}/registration`)
           .send({ firstName, lastName })
-          // .set('Accept', 'application/json')
       );
 
-      console.log('@@AWLC 2');
-
       expect(response.statusCode).toBe(204);
-
-      console.log('@@AWLC 3');
 
       const sierraUser = await clients.sierra.getPatronRecordByRecordNumber(
         testUser.userId
       );
-
-      console.log('@@AWLC 4');
 
       if (sierraUser.status === ResponseStatus.Success) {
         expect(sierraUser.result.firstName).toBe(firstName);
@@ -37,21 +28,19 @@ describe('/users/{userId}/registration', () => {
       } else {
         throw new Error(`Unexpected failure from Sierra: ${sierraUser}`);
       }
-
-      console.log('@@AWLC 5');
     });
 
-    // it('404s for users that do not exist', async () => {
-    //   const { api } = mockedApi();
-    //   const id = 6666666;
-    //   const response = await withCallerId('@machine')(
-    //     api
-    //       .put(`/users/${id}/registration`)
-    //       .send({ newPassword: 'firstName', password: 'lastName' })
-    //       .set('Accept', 'application/json')
-    //   );
+    it('404s for users that do not exist', async () => {
+      const { api } = mockedApi();
+      const id = 6666666;
+      const response = await withCallerId('@machine')(
+        api
+          .put(`/users/${id}/registration`)
+          .send({ newPassword: 'firstName', password: 'lastName' })
+          .set('Accept', 'application/json')
+      );
 
-    //   expect(response.statusCode).toBe(404);
-    // });
+      expect(response.statusCode).toBe(404);
+    });
   });
 });
