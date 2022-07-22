c0b86367c8ec243e805ac3476d24ed8a4eff19e3
Alex Chan <alex@alexwlchan.net>
2022-07-22 09:54:37 +0100

    Add a test for empty field tag n

--- packages/shared/sierra-client/tests/marc/names.test.ts
+++ packages/shared/sierra-client/tests/marc/names.test.ts
@@ -1,8 +1,20 @@
-import { getPatronName } from '../../src/marc';
+import { getPatronName, varFieldTags } from '../../src/marc';
 
 describe('getPatronName', () => {
   it('returns an empty name if there are no matching varfields', () => {
     const name = getPatronName([]);
+
+    expect(name.firstName).toBe('');
+    expect(name.lastName).toBe('');
+  });
+
+  it('returns an empty name if the contents of field tag n is empty', () => {
+    const name = getPatronName([
+      {
+        fieldTag: varFieldTags.name,
+        content: ' ',
+      },
+    ]);
 
     expect(name.firstName).toBe('');
     expect(name.lastName).toBe('');
