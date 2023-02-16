60f92866d430c731327d5cdefe5261c6979365ae
jamie <j.parkinson@wellcome.ac.uk>
2023-02-16 17:19:56 +0000

    More tests to fix

--- packages/apps/auth0-actions/tests/redirect_to_full_registration.test.ts
+++ packages/apps/auth0-actions/tests/redirect_to_full_registration.test.ts
@@ -1,7 +1,4 @@
-import {
-  onContinuePostLogin,
-  onExecutePostLogin,
-} from '../src/redirect_to_full_registration';
+import { onExecutePostLogin } from '../src/redirect_to_full_registration';
 import {
   API,
   EncodedToken,
@@ -18,7 +15,6 @@ describe('redirect_to_full_registration', () => {
     IDENTITY_APP_BASEURL: 'stage.wellcomecollection.org',
     AUTH0_PAYLOAD_SECRET: 'ABCDEFG1234',
   };
-  const testRequest = { hostname: 'stage.wellcomecollection.org' };
 
   beforeEach(() => {
     jest.clearAllMocks();
@@ -43,8 +39,6 @@ describe('redirect_to_full_registration', () => {
       secret: 'ABCDEFG1234',
       payload: {
         email: 'raviravioli@pastatimes.com',
-        iss: `https://${testRequest.hostname}/`,
-        sub: 'p|12345',
       },
     };
 
@@ -52,7 +46,6 @@ describe('redirect_to_full_registration', () => {
       query: {
         session_token:
           mockPostLoginApi.redirect.encodeToken(encodeTokenPayload),
-        redirect_uri: `https://${testRequest.hostname}/continue`,
       },
     };
 
@@ -64,25 +57,6 @@ describe('redirect_to_full_registration', () => {
       REGISTRATION_FORM_URL,
       sendUserQuery
     );
-  });
-
-  it('sets appMetadata terms_and_conditions_accepted to true once form is submitted to /continue', () => {
-    const user: Auth0User = {
-      app_metadata: {},
-    } as Auth0User;
-    const continueNewUserEvent = (
-      submittedFullRegistrationUser: Auth0User
-    ): Event<Auth0User> =>
-      ({
-        user: submittedFullRegistrationUser as Auth0User,
-        tenant: { id: 'tenant_stage' } as EventTenant,
-        request: testRequest as EventRequest,
-        secrets: testSecrets as EventSecrets,
-      } as Event<Auth0User>);
-
-    const payload = { terms_and_conditions_accepted: true };
-    onContinuePostLogin(continueNewUserEvent(user), mockPostLoginApi);
-    expect(mockPostLoginApi.redirect.validateToken).toReturnWith(payload);
   });
 });
 
