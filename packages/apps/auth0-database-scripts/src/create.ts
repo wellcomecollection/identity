f45b85f1111a46c91d5046614b21fc6431e9a273
jamie <j.parkinson@wellcome.ac.uk>
2023-02-16 16:13:08 +0000

    We can make one less request here

--- packages/apps/auth0-database-scripts/src/create.ts
+++ packages/apps/auth0-database-scripts/src/create.ts
@@ -21,14 +21,9 @@ const tempLastName = `${REGISTRATION_NAME_PREFIX}_tempLastName`;
 async function create(user: Auth0UserWithPassword) {
   // We need to create the patron in sierra, we will update the patron info with firstName, lastName etc
   // when we get this information from the full registration form
-  console.log('CREATE FUNCTION BEGINS');
-
   const apiRoot = configuration.API_ROOT;
   const clientKey = configuration.CLIENT_KEY;
   const clientSecret = configuration.CLIENT_SECRET;
-
-  const tempFirstName = `Auth0_Registration_${user.user_id}`;
-  const tempLastName = 'Auth0_Registration_tempLastName';
 
   const sierraClient = new HttpSierraClient(apiRoot, clientKey, clientSecret);
 
@@ -41,13 +36,9 @@ async function create(user: Auth0UserWithPassword) {
     user.password
   );
 
-  console.log(
-    `Received create patron response = ${JSON.stringify(createPatronResponse)}`
-  );
-
   if (createPatronResponse.status === ResponseStatus.UserAlreadyExists) {
-    console.log('CREATE PATRON IN SIERRA ERRORS - USER ALREADY EXISTS');
-    throw new ValidationError(user.email, userAlreadyExistsMessage);
+    // https://auth0.com/docs/authenticate/database-connections/custom-db/templates/create#return-error-that-user-already-exists
+    throw new ValidationError('user_exists', userAlreadyExistsMessage);
   }
 
   if (createPatronResponse.status !== ResponseStatus.Success) {
@@ -72,29 +63,18 @@ async function create(user: Auth0UserWithPassword) {
     ) {
       throw new ValidationError(user.email, 'Please use a shorter password.');
     } else {
-      console.log('CREATE PATRON IN SIERRA ERRORS');
       throw new Error(createPatronResponse.message);
     }
   }
 
-  // We now need to find the patron we created and eventually update their barcode
-  const findPatronResponse = await sierraClient.getPatronRecordByEmail(
-    user.email
-  );
-  if (findPatronResponse.status !== ResponseStatus.Success) {
-    console.log('FIND PATRON BARCODE - NOT SUCCESSFUL');
-    throw new Error(findPatronResponse.message);
-  }
-  const { recordNumber } = findPatronResponse.result;
-
   // Now we update the patron record with a barcode based on the recordNumber
   // We make the recordNumber (patron id) the barcode, sierra expects barcode to be a string
+  const recordNumber = createPatronResponse.result.recordNumber;
   const updatePatronBarcodeResponse = await sierraClient.updatePatron(
     recordNumber,
     { barcodes: [recordNumber.toString()] }
   );
   if (updatePatronBarcodeResponse.status !== ResponseStatus.Success) {
-    console.log('UPDATE PATRON BARCODE NOT SUCCESSFUL');
     throw new Error(updatePatronBarcodeResponse.message);
   }
 }
