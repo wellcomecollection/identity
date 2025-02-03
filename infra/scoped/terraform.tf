f4a0e61ec94adf526388cfb7a62f793f233b7caa
Agnes Garoux <a.garoux@wellcome.ac.uk>
2025-02-03 15:36:12 +0000

    lint

--- infra/scoped/terraform.tf
+++ infra/scoped/terraform.tf
@@ -89,8 +89,8 @@ data "terraform_remote_state" "monitoring" {
     assume_role = {
       role_arn = "arn:aws:iam::760097843905:role/platform-read_only"
     }
-    bucket   = "wellcomecollection-platform-infra"
-    key      = "terraform/monitoring.tfstate"
-    region   = "eu-west-1"
+    bucket = "wellcomecollection-platform-infra"
+    key    = "terraform/monitoring.tfstate"
+    region = "eu-west-1"
   }
 }
