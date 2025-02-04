cfec615037d805eeb3f7b70db50bf9320c83052a
Robert Kenny <r.kenny@wellcome.org>
2025-02-04 10:14:45 +0000

    persuade lint0staged /app is sfe

--- .buildkite/scripts/linting.sh
+++ .buildkite/scripts/linting.sh
@@ -3,8 +3,10 @@ set -o errexit
 
 # Versions should match package.json
 yarn global add lint-staged@13.0.2 prettier@2.2.1
+
+git config --global --add safe.directory /app
+
 lint-staged --diff="main..$BUILDKITE_BRANCH"
 
 # Fail if any changes were made by linter
-git config --global --add safe.directory /app
 git diff --cached --exit-code
