cd3a96018e61f2c15d527ed25b58158aed9b40aa
Robert Kenny <r.kenny@wellcome.org>
2025-02-04 09:15:59 +0000

    persuade linter git is there

--- .buildkite/scripts/linting.sh
+++ .buildkite/scripts/linting.sh
@@ -6,4 +6,5 @@ yarn global add lint-staged@13.0.2 prettier@2.2.1
 lint-staged --diff="main..$BUILDKITE_BRANCH"
 
 # Fail if any changes were made by linter
+git config --global --add safe.directory /app
 git diff --cached --exit-code
