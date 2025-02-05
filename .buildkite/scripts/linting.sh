229e71041b044269e0a07394c8bc6b0a27a34a46
Robert Kenny <r.kenny@wellcome.org>
2025-02-05 11:37:08 +0000

    remove unused git config

--- .buildkite/scripts/linting.sh
+++ .buildkite/scripts/linting.sh
@@ -3,9 +3,6 @@ set -o errexit
 
 # Versions should match package.json
 yarn global add lint-staged@13.0.2 prettier@2.2.1
-
-git config --global --add safe.directory /app
-
 lint-staged --diff="main..$BUILDKITE_BRANCH"
 
 # Fail if any changes were made by linter
